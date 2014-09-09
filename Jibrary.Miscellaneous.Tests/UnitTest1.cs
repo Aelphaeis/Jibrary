b10656e005e96ab54f3c352ad5ae61f171e84244
Joseph <aelphaeis@live.com>
2014-09-08 20:08:57 -0400

    Added Singleton Class

---
+++ Jibrary.Miscellaneous.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Jibrary.Miscellaneous;
+using Jibrary.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jibrary.Miscellaneous.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Singleton<TimeAnalyst>.Instance.CreateTask("Hello World");
+            Console.WriteLine(Singleton<TimeAnalyst>.Instance);
+            Singleton<TimeAnalyst>.Instance.RemoveTask("Hello World");
+            Console.WriteLine(Singleton<TimeAnalyst>.Instance);
+        }
+    }
+}
---
+++ Jibrary.Miscellaneous/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibrary.Miscellaneous
+{
+    public class Singleton<T> where T : new()
+    {
+        public static T Instance { get { return instance; } }
+        static T instance = new T();
+    }
+}
