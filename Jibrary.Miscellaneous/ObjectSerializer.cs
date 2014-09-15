a750e2eab0a38c0baec94c76269bdf999f607619
joseph-plex <joseph@plexxis.com>
2014-09-15 09:17:23 -0400

    Added ObjectSerializer

---
+++ Jibrary.Miscellaneous/ObjectSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace Jibrary.Miscellaneous
+{
+    public class StringSerializer
+    {
+        public String Serialize(Object obj)
+        {
+            using(StringWriter writer = new StringWriter())
+            {
+                new XmlSerializer(obj.GetType()).Serialize(writer, obj);
+                return obj.ToString();
+            }
+        }
+
+        public T Deserialize<T>(String xml)
+        {
+            using(TextReader reader = new StringReader(xml))
+                return (T)new XmlSerializer(typeof(T)).Deserialize(reader);
+        }
+    }
+}
