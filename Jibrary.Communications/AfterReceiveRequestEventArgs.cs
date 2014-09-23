50537872cafb4b81ef234cbbabb7459d28821377
Aelphaeis <aelphaeis@live.com>
2014-09-23 13:05:35 -0400

    Added Communication Lib.

---
+++ Jibrary.Communications/AfterReceiveRequestEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace Jibrary.Communications
+{
+    public class AfterReceiveRequestEventArgs : EventArgs
+    {
+        public Message Request { get; set; }
+        public IClientChannel Channel { get; set; }
+        public InstanceContext InstanceContext { get; set; }
+
+    }
+}
---
+++ Jibrary.Communications/BeforeSendReplyEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ServiceModel.Channels;
+
+namespace Jibrary.Communications
+{
+    public class BeforeSendReplyEventArgs : EventArgs
+    {
+        public Message Reply { get; set; }
+        public Object CorrelationState { get; set; }
+    }
+}
