c96933cce29a7999a38efcdc9477614e55ab07fe
Aelphaeis <aelphaeis@live.com>
2014-09-23 14:49:28 -0400

    Added Documentation

---
+++ Jibrary.Communications.Tests/Resources/TestService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace Jibrary.Communications.Tests.Resources
+{
+    [ServiceBehavior]
+    public class TestService : ITestService
+    {
+        [OperationBehavior]
+        public String GetData(Int32 value)
+        {
+            return String.Format("You entered: {0}", value);
+        }
+    }
+
+    [ServiceContract]
+    public interface ITestService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
